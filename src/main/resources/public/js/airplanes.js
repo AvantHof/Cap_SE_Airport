93803ea7575d4718e750fdad21ef7ac366fdfa9f
Alexander van 't Hof <alexander.vthof@gmail.com>
2018-02-01 11:15:14 +0100

    Front-end shows datatable with airplanes

--- src/main/resources/public/js/airplanes.js
+++ src/main/resources/public/js/airplanes.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    $('#airplaneTable').DataTable();
+    $('#airplaneTable').DataTable({
+        columns: [
+            { "data": "name" },
+            { "data": "gas" }
+        ]
+    });
+
+    updateTable();
 } );
+
+function updateTable() {
+    console.log("Updating table..");
+
+    $.ajax({
+        url:"/api/airplane/",
+        type:"GET",
+        success: function(airplane) {
+          $('#airplaneTable').DataTable().clear();
+          $('#airplaneTable').DataTable().rows.add(airplane);
+          $('#airplaneTable').DataTable().columns.adjust().draw();
+        }
+    });
+}
